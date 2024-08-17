d31b0e46a5a3fa7dd0cb145a52b5ec37ae19d05c
Mahad Saffi <mahadsaffi@gmail.com>
2024-08-17 22:04:47 +0500

    Update in utils

--- src/index.js
+++ src/index.js
@@ -5,4 +5,21 @@ dotenv.config({
     path: "./env"
 });
 
-connectDB();
+connectDB()
+.then(() => {
+    console.log(`MongoDB connected successfully`);
+
+    // Listening for any type of error
+    app.on("error", (err) => {
+        console.log(`Express server error: ${err}`);
+    });
+
+    // Listening for the port
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+})
+.catch((err) => {
+    console.log(`MongoDB connection failed: ${err}`);
+    process.exit(1);
+});
