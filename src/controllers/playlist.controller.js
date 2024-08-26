c7076da96a294e3adc347470cf5be13204edbeba
Mahad Saffi <mahadsaffi@gmail.com>
2024-08-26 21:14:08 +0500

    add subscriptions controllers

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -32,6 +32,22 @@ const createPlaylist = asyncHandler(async (req, res) => {
 const getUserPlaylists = asyncHandler(async (req, res) => {
     const {userId} = req.params
     //TODO: get user playlists
+
+    if (!userId) {
+        throw new ApiError(400, "User id is required")
+    }
+
+    const platlists = await Playlist.find({owner: userId})
+
+    if (!platlists) {
+        throw new ApiError(500, "Unable to find playlists")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, platlists, "Playlists fetched successfully")
+    )
 })
 
 const getPlaylistById = asyncHandler(async (req, res) => {
@@ -60,19 +76,88 @@ const addVideoToPlaylist = asyncHandler(async (req, res) => {
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
     const {playlistId, videoId} = req.params
-    // TODO: remove video from playlist
+    
+    if (!playlistId || !videoId) {
+        throw new ApiError(400, "Playlist id and video id is required")
+    }
 
+    const playlist = await Playlist.findById(playlistId)
+
+    if (!playlist) {
+        throw new ApiError(500, "Unable to find playlist")
+    }
+
+    const videoIndex = playlist.videos.indexOf(videoId)
+
+    if (videoIndex === -1) {
+        throw new ApiError(404, "Video not found in playlist")
+    }
+
+    playlist.videos.splice(videoIndex, 1)
+
+    const updatedPlaylist = await playlist.save()
+
+    if (!updatedPlaylist) {
+        throw new ApiError(500, "Unable to remove video from playlist")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, updatedPlaylist, "Video removed from playlist successfully")
+    )
 })
 
 const deletePlaylist = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
-    // TODO: delete playlist
+
+    if (!playlistId) {
+        throw new ApiError(400, "Playlist id is required")
+    }
+
+    const playlist = await Playlist.findById(playlistId)
+
+    if (!playlist) {
+        throw new ApiError(404, "Playlist not found")
+    }
+
+    await playlist.remove()
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, {} ,"Playlist deleted successfully")
+    )
 })
 
 const updatePlaylist = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     const {name, description} = req.body
-    //TODO: update playlist
+
+    if (!playlistId) {
+        throw new ApiError(400, "Playlist id is required")
+    }
+
+    const playlist = await Playlist.findById(playlistId)
+
+    if (!playlist) {
+        throw new ApiError(404, "Playlist not found")
+    }
+
+    playlist.name = name || playlist.name
+    playlist.description = description || playlist.description
+
+    const updatedPlaylist = await playlist.save()
+
+    if (!updatedPlaylist) {
+        throw new ApiError(500, "Unable to update playlist")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, updatedPlaylist, "Playlist updated successfully")
+    )
 })
 
 export {
