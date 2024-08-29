ce43127c40f83df624938fc24e31e8e02df5d937
Mahad Saffi <mahadsaffi@gmail.com>
2024-08-29 19:22:33 +0500

    complete dashboard controllers

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -7,11 +7,66 @@ import ApiResponse from "../utils/ApiResponse.js"
 import asyncHandler from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
-    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+
+    const { userId } = req.user?._id
+
+    if (!userId) {
+        throw new ApiError(400, "User id is required")
+    }
+
+    const videos = await Video.find({ owner: userId })
+
+    if (!videos) {
+        throw new ApiError(500, "Unable to find videos")
+    }
+
+    const totalVideos = videos.length
+
+    const totalViews = videos.reduce((acc, video) => acc + video.views, 0)
+
+    const totalLikes = await Like.find({ video: { $in: videos.map(video => video._id) } })
+
+    if (!totalLikes) {
+        throw new ApiError(500, "Unable to find likes")
+    }
+
+    const totalSubscribers = await Subscription.find({ channel: userId })
+
+    if (!totalSubscribers) {
+        throw new ApiError(500, "Unable to find subscribers")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, {
+            totalVideos,
+            totalViews,
+            totalLikes: totalLikes.length,
+            totalSubscribers: totalSubscribers.length
+        }, "Channel stats fetched successfully")
+    )
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
-    // TODO: Get all the videos uploaded by the channel
+
+    const { userId } = req.user?._id
+
+    if (!userId) {
+        throw new ApiError(400, "User id is required")
+    }
+
+    const videos = await Video.find({ owner: userId })
+
+    if (!videos) {
+        throw new ApiError(500, "Unable to find videos")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, videos, "Videos fetched successfully")
+    )
 })
 
 export {
